e89ab6972b82d956afe4f11c54c3b34337972c9c
Javier Perez <123135076+jjperez7@users.noreply.github.com>
2023-11-16 15:34:13 -0800

    Update led.sv

--- led.sv
+++ led.sv
@@ -1,7 +1,7 @@
 module top(input logic reset,
-  input logic signal,
-  output logic [7:0] row,
-  output logic [7:0] col);
+		   input logic [1:0] signal,
+		   output logic [7:0] row,
+		   output logic [7:0] col);
  
 // Top most module that combines all sub-modules within
 // to create the whole circuit, additional
@@ -12,7 +12,7 @@ module top(input logic reset,
 //comments in each sub module
 
 logic [23:0] counter;
-logic [7:0] r0, c0, r1, c1;
+logic [7:0] r0, c0, r1, c1, r2, c2, r3, c3;
 
 // Internal high-speed oscillator
 HSOSC #(.CLKHF_DIV(2'b01))
@@ -28,19 +28,18 @@ else            counter <= counter + 1;
 //sub-modules
 ledFSM0 FSM0(reset, counter[16], r0, c0);
 ledFSM1 FSM1(reset, counter[16], r1, c1);
-mux8 colMux(signal, c0, c1, col); 
-mux8 rowMux(signal, r0, r1, row); 
+ledFSM2 FSM2(reset, counter[16], r2, c2);
+ledFSM3 FSM3(reset, counter[16], r3, c3);
+/*
+mux2_8b colMux(signal[0], c1, c0, col); 
+mux2_8b rowMux(signal[0], r1, r0, row); 
+*/
+mux4_8b colMux(c0, c1, c3, c2, signal, col); 
+mux4_8b rowMux(r0, r1, r3, r2, signal, row); 
 endmodule
 
 /*
-module ledDecoder(output logic [7:0] row,
- output logic [7:0] col);
-
-assign row = 8'b11100111;
-assign col = 8'b00011000;
-endmodule
-*/
-module mux8 (input signal,
+module mux2_8b (input signal,
 input logic [7:0] s0,
 input logic [7:0] s1,
 output logic [7:0] sC);
@@ -55,11 +54,18 @@ output logic [7:0] sC);
 
 assign sC = signal ? s0:s1;
 endmodule
+*/
+module mux4_8b(input logic [7:0] d0, d1, d2, d3,
+			input logic [1:0] s,
+			output logic [7:0] y);
+
+assign y = s[1] ? (s[0] ? d3 : d2) : (s[0] ? d1 : d0);
+endmodule
 
 module ledFSM0 (input logic reset,
-input logic clk,
-output logic [7:0] row,
-output logic [7:0] col);
+				 input logic clk,
+				 output logic [7:0] row,
+				 output logic [7:0] col);
  
 /* FSM that says whether or not a key is pressed or not.
    If key pressed and other key also pressed after,
@@ -105,9 +111,9 @@ assign row[7] = (state == S0 | state == S1 | state == S2);
 endmodule
 
 module ledFSM1(input logic reset,
-input logic clk,
-output logic [7:0] row,
-output logic [7:0] col);
+				 input logic clk,
+				 output logic [7:0] row,
+				 output logic [7:0] col);
  
 /* FSM that says whether or not a key is pressed or not.
    If key pressed and other key also pressed after,
@@ -141,6 +147,104 @@ assign col[4] = (state == S0);
 assign col[5] = (state == S0);
 assign col[6] = (state == S1);
 assign col[7] = (state == S2);
+
+assign row[0] = (state == S0 | state == S1 | state == S2);
+assign row[1] = (state == S0 | state == S1);
+assign row[2] = (state == S0 | state == S2);
+assign row[3] = (state == S1 | state == S2);
+assign row[4] = (state == S1 | state == S2);
+assign row[5] = (state == S0 | state == S2);
+assign row[6] = (state == S0 | state == S1);
+assign row[7] = (state == S0 | state == S1 | state == S2);
+endmodule
+
+module ledFSM2(input logic reset,
+				 input logic clk,
+				 output logic [7:0] row,
+				 output logic [7:0] col);
+ 
+/* FSM that says whether or not a key is pressed or not.
+   If key pressed and other key also pressed after,
+   will not acknowedlge new key press
+*/
+
+typedef enum logic [1:0] {S0, S1, S2} statetype;
+statetype state, nextstate;
+
+
+// state register
+always_ff @(posedge clk)
+if (reset == 0) state <= S0;
+else state <= nextstate;
+
+// next state logic
+always_comb
+case (state)
+S0: nextstate = S1;
+S1: nextstate = S2;
+S2: nextstate = S0;
+default: nextstate = S0;
+endcase
+
+// output logic
+assign col[0] = 0;
+assign col[1] = 0;
+assign col[2] = 0;
+assign col[3] = (state == S2);
+assign col[4] = (state == S1);
+assign col[5] = (state == S0);
+assign col[6] = (state == S0);
+assign col[7] = (state == S1);
+
+
+assign row[0] = (state == S0 | state == S1 | state == S2);
+assign row[1] = (state == S0 | state == S1);
+assign row[2] = (state == S0 | state == S2);
+assign row[3] = (state == S1 | state == S2);
+assign row[4] = (state == S1 | state == S2);
+assign row[5] = (state == S0 | state == S2);
+assign row[6] = (state == S0 | state == S1);
+assign row[7] = (state == S0 | state == S1 | state == S2);
+endmodule
+
+module ledFSM3(input logic reset,
+				 input logic clk,
+				 output logic [7:0] row,
+				 output logic [7:0] col);
+ 
+/* FSM that says whether or not a key is pressed or not.
+   If key pressed and other key also pressed after,
+   will not acknowedlge new key press
+*/
+
+typedef enum logic [1:0] {S0, S1, S2} statetype;
+statetype state, nextstate;
+
+
+// state register
+always_ff @(posedge clk)
+if (reset == 0) state <= S0;
+else state <= nextstate;
+
+// next state logic
+always_comb
+case (state)
+S0: nextstate = S1;
+S1: nextstate = S2;
+S2: nextstate = S0;
+default: nextstate = S0;
+endcase
+
+// output logic
+assign col[0] = 0;
+assign col[1] = 0;
+assign col[2] = 0;
+assign col[3] = 0;
+assign col[4] = (state == S2);
+assign col[5] = (state == S1);
+assign col[6] = (state == S0);
+assign col[7] = (state == S0);
+
 
 assign row[0] = (state == S0 | state == S1 | state == S2);
 assign row[1] = (state == S0 | state == S1);
