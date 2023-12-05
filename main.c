60555dad615e4869c8213ee937dc3b6a8f00a023
Dimitri Avila <91030962+mitri-afk@users.noreply.github.com>
2023-12-05 13:46:45 -0800

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ Purpose : Generic application start
 #include "STM32L432KC_FLASH.h"
 #include "stm32l432xx.h"
 #include "STM32L432KC_GPIO.h"
+#include "stdio.h"
 
 /*********************************************************************
 *
@@ -26,23 +27,317 @@ Purpose : Generic application start
 
 
 #define GPIO GPIOA
+
 void helper(void);
 void delay(int cycles);
 void setStep(int w1, int w2, int w3, int w4);
 void forward(int steps, int delay);
-#define DELAY_DURATION  300
+#define DELAY_DURATION  200
 
-int main(void) {
+
+
+void spinFlap1(){
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 1);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 1);
+    digitalWrite(PA0, 0);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 1);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 0);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 1);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 0);
+
+    delay(DELAY_DURATION);
+
+}
+void spinFlap2() {
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 1);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 1);
+    digitalWrite(PA5, 0);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 1);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 0);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA2, 1);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 0);
+
+    delay(DELAY_DURATION);
+  
+}
+void spinFlap3(){
+  digitalWrite(PA11, 0);
+  digitalWrite(PA12, 0);
+  digitalWrite(PA10, 0);
+  digitalWrite(PA9, 1);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PA11, 0);
+  digitalWrite(PA12, 0);
+  digitalWrite(PA10, 1);
+  digitalWrite(PA9, 0);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PA11, 0);
+  digitalWrite(PA12, 1);
+  digitalWrite(PA10, 0);
+  digitalWrite(PA9, 0);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PA11, 1);
+  digitalWrite(PA12, 0);
+  digitalWrite(PA10, 0);
+  digitalWrite(PA9, 0);
+
+  delay(DELAY_DURATION);
+
+}
+
+void spinFlap4() {
+  digitalWrite(PB0, 0);
+  digitalWrite(PB5, 0);
+  digitalWrite(PB4, 0);
+  digitalWrite(PB1, 1);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PB0, 0);
+  digitalWrite(PB5, 0);
+  digitalWrite(PB4, 1);
+  digitalWrite(PB1, 0);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PB0, 0);
+  digitalWrite(PB5, 1);
+  digitalWrite(PB4, 0);
+  digitalWrite(PB1, 0);
+
+  delay(DELAY_DURATION);
+
+  digitalWrite(PB0, 1);
+  digitalWrite(PB5, 0);
+  digitalWrite(PB4, 0);
+  digitalWrite(PB1, 0);
+
+  delay(DELAY_DURATION);
+
+
+}
+
+
+// For testing!
+int mainTESTING(void){
   configureFlash();
   configureClock();
-  RCC->AHB2ENR |= (1 << 0); // turn on GPIO_A
-  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN; // turn on GPIO_B
+
+  // Turn on GPIOA and GPIOB clock domains (GPIOAEN and GPIOBEN bits in AHB1ENR)
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN);
+
+  pinMode(PA0, 1);
+  pinMode(PA1, 1);
+  pinMode(PA3, 1);
+  pinMode(PA4, 1);
+
+  pinMode(PA5, 1);
+  pinMode(PA6, 1);
+  pinMode(PA7, 1);
+  pinMode(PA2, 1);
+
+  pinMode(PA9, 1);
+  pinMode(PA10, 1);
+  pinMode(PA12, 1);
+  pinMode(PA11, 1);
+
+  pinMode(PB0, 1);
+  pinMode(PB5, 1);
+  pinMode(PB4, 1);
+  pinMode(PB1, 1);
+
+  while(1) {
+
+    spinFlap4();
+  }
+}
+
+// CODE THAT MADE THE FLAPS WORK! ACTUAL MAIN IS THIS ONE. USE THIS ONE!
+int mainACTUAL(void) {
+  configureFlash();
+  configureClock();
+
+  // Turn on GPIOA and GPIOB clock domains (GPIOAEN and GPIOBEN bits in AHB1ENR)
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN);
+
+  pinMode(PA0, 1);
+  pinMode(PA1, 1);
+  pinMode(PA3, 1);
+  pinMode(PA4, 1);
+
+  pinMode(PA5, 1);
+  pinMode(PA6, 1);
+  pinMode(PA7, 1);
+  pinMode(PA2, 1);
+
+  pinMode(PA9, 1);
+  pinMode(PA10, 1);
+  pinMode(PA12, 1);
+  pinMode(PA11, 1);
+
+  pinMode(PB0, 1);
+  pinMode(PB5, 1);
+  pinMode(PB4, 1);
+  pinMode(PB1, 1);
+
+  int i = 0;
+  while (1){
+
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 1);
+
+    digitalWrite(PA11, 0);
+    digitalWrite(PA12, 0);
+    digitalWrite(PA10, 0);
+    digitalWrite(PA9, 1);
+
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 1);
+
+    digitalWrite(PB0, 0);
+    digitalWrite(PB5, 0);
+    digitalWrite(PB4, 0);
+    digitalWrite(PB1, 1);
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 1);
+    digitalWrite(PA0, 0);
+
+    digitalWrite(PA11, 0);
+    digitalWrite(PA12, 0);
+    digitalWrite(PA10, 1);
+    digitalWrite(PA9, 0);
+
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 1);
+    digitalWrite(PA5, 0);
+
+    digitalWrite(PB0, 0);
+    digitalWrite(PB5, 0);
+    digitalWrite(PB4, 1);
+    digitalWrite(PB1, 0);
+
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 0);
+    digitalWrite(PA3, 1);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 0);
+
+    digitalWrite(PA11, 0);
+    digitalWrite(PA12, 1);
+    digitalWrite(PA10, 0);
+    digitalWrite(PA9, 0);
+
+    digitalWrite(PA2, 0);
+    digitalWrite(PA7, 1);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 0);
+
+    digitalWrite(PB0, 0);
+    digitalWrite(PB5, 1);
+    digitalWrite(PB4, 0);
+    digitalWrite(PB1, 0);
+
+
+    delay(DELAY_DURATION);
+
+    digitalWrite(PA4, 1);
+    digitalWrite(PA3, 0);
+    digitalWrite(PA1, 0);
+    digitalWrite(PA0, 0);
+
+    digitalWrite(PA11, 1);
+    digitalWrite(PA12, 0);
+    digitalWrite(PA10, 0);
+    digitalWrite(PA9, 0);
+
+    digitalWrite(PA2, 1);
+    digitalWrite(PA7, 0);
+    digitalWrite(PA6, 0);
+    digitalWrite(PA5, 0);
+
+    digitalWrite(PB0, 1);
+    digitalWrite(PB5, 0);
+    digitalWrite(PB4, 0);
+    digitalWrite(PB1, 0);
+
+    delay(DELAY_DURATION);
+
+    i += 1;
+
+  }
+}
+
+
+int oldmain(void) {
+  configureFlash();
+  configureClock();
+  //RCC->AHB2ENR |= (1 << 0); // turn on GPIO_A
+  //RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN; // turn on GPIO_B
+
+  // Turn on GPIOA and GPIOB clock domains (GPIOAEN and GPIOBEN bits in AHB1ENR)
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN);
+    
+
   //GPIO->AFRL &= ~(0b1111 << 24);
   //GPIO->AFRL |= (0b1110 << 24);
   pinMode(PA9, 1);
   pinMode(PA10, 1);
   pinMode(PA11, 1);
   pinMode(PA2, 1);
+
 
 
   pinMode(PA3, 1);
@@ -55,6 +350,13 @@ int main(void) {
   pinMode(PA1, 1);
   pinMode(PA7, 1);
   pinMode(PA8, 1);
+
+
+  pinMode(PA12, 1);
+  pinMode(PB7, 1);
+  pinMode(PB1, 1);
+  pinMode(PB0, 1);
+
 
 
   int i = 0;
@@ -77,6 +379,12 @@ int main(void) {
       digitalWrite(PA1, 0);
       digitalWrite(PA7, 0);
       digitalWrite(PA8, 1);
+
+      //digitalWrite(PA12, 0);
+      //digitalWrite(PB7, 0);
+      //digitalWrite(PB6, 0);
+      //digitalWrite(PB1, 1);
+      
       
       
       delay(DELAY_DURATION);
@@ -97,6 +405,12 @@ int main(void) {
       digitalWrite(PA1, 0);
       digitalWrite(PA7, 1);
       digitalWrite(PA8, 0);
+
+      //digitalWrite(PA12, 0);
+      //digitalWrite(PB7, 0);
+      //digitalWrite(PB6, 1);
+      //digitalWrite(PB1, 0);
+      
       
       delay(DELAY_DURATION);
 
@@ -116,6 +430,12 @@ int main(void) {
       digitalWrite(PA1, 1);
       digitalWrite(PA7, 0);
       digitalWrite(PA8, 0);
+
+      //digitalWrite(PA12, 0);
+      //digitalWrite(PB7, 1);
+      //digitalWrite(PB6, 0);
+      //digitalWrite(PB1, 0);
+      
       
       delay(DELAY_DURATION);
 
@@ -135,6 +455,11 @@ int main(void) {
       digitalWrite(PA1, 0);
       digitalWrite(PA7, 0);
       digitalWrite(PA8, 0);
+      
+      //digitalWrite(PA12, 1);
+      //digitalWrite(PB7, 0);
+      //digitalWrite(PB6, 0);
+      //digitalWrite(PB1, 0);
       
         
       delay(DELAY_DURATION);
@@ -242,6 +567,265 @@ void helper(void) {
   }
   delay(DELAY_DURATION);
 }
+
+
+
+
+
+
+// Working on Hall Effect Sensor...
+
+int hallEffect(void){ 
+  // Set pin mode to input
+  pinMode(PB6, 0);
+
+  pinMode(PB0, 1);
+  pinMode(PB5, 1);
+  pinMode(PB4, 1);
+  pinMode(PB1, 1);
+
+  //spinFlap4();
+
+  while(1) {
+    // Continuously read sensor output from pin PB6 
+    int valueLED = digitalRead(PB6);
+    printf("%d value of LED: \n", valueLED);
+    if(digitalRead(PB6) == 0) {
+      // If we detect the magnet, turn off the LED
+      //digitalWrite(PB7, 0);
+      delay(500);
+      //printf("Magnet Detected\n");
+    } else {
+    // If we don't, turn on LED
+    digitalWrite(PB7, 1);
+    //printf("Magnet Not Detected\n");
+    }
+    delay(500);
+  }
+}
+
+//working to splin flap 4 with hall sensor
+int spinFlap4WithHall(void) {
+  configureFlash();
+  configureClock();
+
+  // Turn on GPIOA and GPIOB clock domains (GPIOAEN and GPIOBEN bits in AHB1ENR)
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN);
+
+
+  // Motor pin assignments
+  //pinMode(PA0, 1);
+  //pinMode(PA1, 1);
+  //pinMode(PA3, 1);
+  //pinMode(PA4, 1);
+    
+  //pinMode(PA5, 1);
+  //pinMode(PA6, 1);
+  //pinMode(PA7, 1);
+  //pinMode(PA2, 1);
+    
+  //pinMode(PA9, 1);
+  //pinMode(PA10, 1);
+  //pinMode(PA12, 1);
+  //pinMode(PA11, 1);
+    
+  pinMode(PB0, 1);
+  pinMode(PB5, 1);
+  pinMode(PB4, 1);
+  pinMode(PB1, 1);
+
+  // Set pin mode to input
+  pinMode(PB6, 0);
+
+  while(1) {
+    spinFlap4();
+    int valueLED = digitalRead(PB6);
+    printf("%d value of LED: \n", valueLED);
+    if(digitalRead(PB6) == 0) {
+      // If we detect the magnet, turn off the LED
+      //digitalWrite(PB7, 0);
+
+      // Wait while magnet is detected for a bit for us to see it stop
+      delay(50000);
+
+      // Spin past the magnet a bit so we dont detect it anymore.
+      int i = 0;
+      while(i < 364){ //60
+        spinFlap4();
+        i++;
+      }
+      delay(50000);
+      
+      //printf("Magnet Detected\n");
+    }
+  }
+}
+
+
+int main(void){
+  configureFlash();
+  configureClock();
+
+  // Turn on GPIOA and GPIOB clock domains (GPIOAEN and GPIOBEN bits in AHB1ENR)
+  RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);
+
+
+  // Motor pin assignments
+  pinMode(PA0, 1);
+  pinMode(PA1, 1);
+  pinMode(PA3, 1);
+  pinMode(PA4, 1);
+    
+  pinMode(PA5, 1);
+  pinMode(PA6, 1);
+  pinMode(PA7, 1);
+  pinMode(PA2, 1);
+    
+  pinMode(PA9, 1);
+  pinMode(PA10, 1);
+  pinMode(PA12, 1);
+  pinMode(PA11, 1);
+    
+  pinMode(PB0, 1);
+  pinMode(PB5, 1);
+  pinMode(PB4, 1);
+  pinMode(PB1, 1);
+
+  // Set pin mode to input
+  pinMode(PB7, 0);
+  pinMode(PB6, 0);
+  pinMode(PB3, 0);
+  pinMode(PC14, 0);
+
+  //to test the last flap 
+  while (1) {
+    printf("%d: \n", digitalRead(PB7)); 
+  }
+
+
+  int i = 0;
+  while(i < 364){ //60
+    spinFlap2();
+    //spinFlap3();
+    //spinFlap4();
+    i++;
+  }
+  delay(50000);
+
+  while(1) {
+    spinFlap2();
+    //spinFlap3();
+    //spinFlap4();
+
+    int valueLED3 = digitalRead(PB7);
+    int valueLED4 = digitalRead(PB6);
+
+    while (1) {
+
+      while(digitalRead(PB3) != 0) {
+        spinFlap2();
+        printf("%d split flap 2: \n", digitalRead(PB3));
+      }
+
+      delay(50000);
+      int j = 0;
+      while (j < 140) {
+        spinFlap2();
+        j++;
+
+      }
+
+    }
+
+
+    while((digitalRead(PB7) != 0)) {
+      spinFlap3();
+      printf("%d split flap 3: \n", digitalRead(PB7));
+    }
+
+    while(digitalRead(PB6) != 0) {
+      spinFlap4();
+      printf("%d split flap 4: \n", digitalRead(PB6));
+    }
+
+    printf("Reached here");
+    delay(50000);
+
+    while (1) {
+      spinFlap2();
+      //spinFlap3();
+      //spinFlap4();
+
+    }
+
+    //printf("%d value of LED for Flap 3: \n", valueLED3);
+    //printf("%d value of LED for Flap 4: \n", valueLED4);
+
+    //// While flaps are not home
+    //while((digitalRead(PB7) && digitalRead(PB6)) != 0) {
+    //  // Spin the flaps
+    //  spinFlap3();
+    //  spinFlap4();
+
+    //  // If flap 3 or flap 4 is home
+    //  if(digitalRead(PB7) == 0 | digitalRead(PB6) == 0) {
+    //    // Spin flap 4 if not at home
+    //    while(digitalRead(PB6) != 0) {
+    //      //printf("%d value of LED for Flap 4: \n", valueLED4);
+    //      spinFlap4();
+    //    }
+    //    // Spin flap 3 if not at home
+    //    while(digitalRead(PB7) != 0) {
+    //      printf("%d value of LED for Flap 3: \n", valueLED3);
+    //      printf("%d value of LED for Flap 4: \n", valueLED4);
+
+    //      spinFlap3();
+    //    }
+    //  }
+
+    //// When all are home. Pause for a bit.
+    //delay(50000);
+    //int i = 0;
+    //while(i < 1000) {
+    //  spinFlap3();
+    //  spinFlap4();
+    //  ++i;
+    //}
+ // }
+
+
+
+    //if(digitalRead(PB7) == 0) {
+    //  // Wait while magnet is detected for a bit for us to see it stop
+    //  delay(50000);
+
+    //  // Spin past the magnet a bit so we dont detect it anymore.
+    //  int i = 0;
+    //  while(i < 364){
+    //    spinFlap3();
+    //    i++;
+    //  }
+    //   delay(50000);
+    //}
+
+    //if(digitalRead(PB6) == 0) {
+    //  // Wait while magnet is detected for a bit for us to see it stop
+    //  delay(50000);
+
+    //  // Spin past the magnet a bit so we dont detect it anymore.
+    //  int i = 0;
+    //  while(i < 364){
+    //    spinFlap4();
+    //    i++;
+    //  }
+    //   delay(50000);
+    //}
+  }
+}
+
+
+
+
 
 /*************************** End of file ****************************/
 
